119c28896eacb5e94cd6e697671d4d69446655f7
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2022-11-28 11:12:57 +0530

    .

--- components/PCP/components/Filter_sort/index.tsx
+++ components/PCP/components/Filter_sort/index.tsx
@@ -10,7 +10,7 @@ import DivTitle from './common/DivTitle'
 const Filter_sort = () => {
   return (
    <>
-   <div  className='absolute mr-6 right-1 top-44  bg-gray-100 border border-gray min-h-screen  border-b-2   '>
+   <div  className='absolute mr-6 right-1 top-44  bg-gray-100 border border-gray min-h-screen  border-b-2 hover:shadow-2xl'>
      
      <DivTitle heading={"Filter & sort"}/>
      <div className='text-right border-b-2'> 
