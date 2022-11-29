5d67d49cbe486d3f08937b720e2b67618cc27d49
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2022-11-29 12:33:56 +0530

    Load-More

--- components/ui/InfiniteScroll/index.tsx
+++ components/ui/InfiniteScroll/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 export default function InfiniteScrollComponent({
@@ -5,16 +6,31 @@ export default function InfiniteScrollComponent({
   total,
   fetchData,
   currentNumber,
-}: any) {
+}: any)
+
+{
+  console.log(currentNumber>total)
+  const [show, setshow] = useState(false)
   return (
     <InfiniteScroll
       dataLength={total} //This is important field to render the next data
       next={fetchData}
-      hasMore={currentNumber < total}
+      hasMore={show}
       loader={null}
       endMessage={
-        <p className="py-5" style={{ textAlign: 'center' }}>
-          <b>You have seen it all</b>
+        <p
+        //  className="py-5"
+        // className='flex justify-center pt-6'
+        style={{ display: 'flex' , justifyContent:"center" ,paddingTop: "1.5rem" }}>
+          {/* <b>You have seen it all</b> */}
+          <button
+          // onChange={()=>setshow(false)}
+          onClick={()=>(currentNumber<total?
+          (  setshow(true),setTimeout(() => {
+            setshow(false)
+          }, 1000))
+            :setshow(false))}
+          className='border border-black py-2 px-8 text-gray-700 hover:text-white hover:bg-black font'>Load more</button>
         </p>
       }
     >
