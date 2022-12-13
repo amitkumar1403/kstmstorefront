904500f0dc7dbb2c91d5132453e96a6cd8d96435
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2022-12-13 12:08:04 +0530

    load-more changes

--- components/ui/InfiniteScroll/index.tsx
+++ components/ui/InfiniteScroll/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { Alert, Button } from "@material-tailwind/react";
 
 export default function InfiniteScrollComponent({
   component,
@@ -11,7 +12,58 @@ export default function InfiniteScrollComponent({
 {
   console.log(currentNumber>total)
   const [show, setshow] = useState(false)
+  const [altertShow, setAlertShow] = useState(false);
+  useEffect(() => {
+    // handleClick()
+  
+    return () => {
+      setshow(false)
+    }
+  }, [])
+
+  // const handleClick=()=>{
+  //   currentNumber<total?
+  //        (  setshow(true),setTimeout(() => {
+  //           setshow(false)
+  //         }, 1000))
+  //           :null  
+  //         }
+  const handleClick=()=>{
+    if(currentNumber<total)
+    {
+      console.log(":inside If")
+        setshow(true)
+        setTimeout(() => {
+          console.log("timeout called")
+        
+         setshow(false)
+        }, 1000)
+        return
+    }
+    console.log("else simple return ")
+    setAlertShow(true)
+  
+    return
+   }
+  
   return (
+    <>
+    <div className='flex justify-end m-2'>
+
+    <div className={`fixed z-50 bg-transparent border-blue-500 text-blue-700 px-3 py-2  ${altertShow && `bg-gray-200`}`} role="alert">
+    {/* <Alert>A simple alert for showing message.</Alert> */}
+      <Alert
+        show={altertShow}
+        dismissible={{
+          onClose: () => setAlertShow(false),
+        }}
+      >
+      Please wait || You have seen all the products
+      </Alert>
+    {/* <p className="font-bold">Informational message</p>
+    <p className="text-sm">Some additional text to explain said message.</p> */}
+    </div>
+    </div>
     <InfiniteScroll
       dataLength={total} //This is important field to render the next data
       next={fetchData}
@@ -24,18 +76,17 @@ export default function InfiniteScrollComponent({
         <p
         className='flex justify-center pt-6'
        >
-          <button
-          onClick={()=>(currentNumber<total?
-          (  setshow(true),setTimeout(() => {
-            setshow(false)
-          }, 500))
-            :null)}                                                                                                 //12<70
-          className={`border border-black py-2 px-8 text-gray-700 hover:text-white hover:bg-black }`}>
+          <button  
+          onClick={handleClick}                                                                                         
+          className={`border border-black py-2 px-8 text-gray-700 hover:text-white hover:bg-black ` }   >
+            {/* 12<70 */}
+            {/* !true */}
           {!(currentNumber<total)?`Loading ...`:`Load More`}</button>
         </p>
       }
-    >
+      >
       {component}
     </InfiniteScroll>
+      </>
   )
 }
