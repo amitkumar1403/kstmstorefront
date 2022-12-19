4859d62b216f4def77065bea40b9f2649b3ba828
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2022-12-19 19:00:21 +0530

    on-hover-color-link-fix

--- components/product/ProductCard/AttributeSelector.tsx
+++ components/product/ProductCard/AttributeSelector.tsx
@@ -18,15 +18,11 @@ const ColorSelector = ({ attr, onChange, link }: any) => {
     <div className="mb-1 grid grid-cols-10 lg:max-w-xs m-auto lg:px-10">
       {attr.fieldValues.map((item: any, idx: number) => {
         return (
-          <Link key={idx} href={link || '#'} passHref>
-            <a
-              key={idx}
-              onMouseEnter={() => onChange(item, 'enter')}
-              onMouseLeave={() => onChange(item, 'leave')}
+          <span
+              key={idx}            
               className="sm:h-5 sm:w-5 h-5 w-5 inline-block rounded-full sm:mr-2 mr-1 mt-2 border border-gray-200 shadow-md drop-shadow-md"
               style={{ backgroundColor: item.fieldValue }}
             />
-          </Link>
         )
       })}
     </div>
