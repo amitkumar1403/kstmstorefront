1fd87d7214ce79290f2f64daf31ab059964e1120
Amit Kumar <amit.kumar@bettercommerce.io>
2022-12-20 11:54:25 +0530

    Update index.tsx

--- pages/my-account/index.tsx
+++ pages/my-account/index.tsx
@@ -57,90 +57,89 @@ function MyAccount({ defaultView, isLoggedIn }: any) {
 
   return (
     <section className="relative pb-10 text-gray-900">
-    <div className="w-full px-0 mx-auto accunt-main-container sm:px-0 lg:px-0">
-      <div className="grid w-full grid-cols-12 px-4 sm:px-2 sm:pr-0 main-account-grid pt-16">
-        <div className="col-span-12 lg:col-span-4 md:col-span-4 pl-16 border-r border-gray-200 sm:pl-6 tab-list-sm sm:pt-10">
-           
-                <div className="sticky left-0 z-10 flex flex-col top-36">
-                  {config.map((item: any, idx: number) => (
-                    <>
-                      <div
-                        key={`my-acc-${idx}`}
-                        // href="#"
-                        className={`hover:bg-white hover:text-indigo-600 border border-transparent text-md leading-3 font-medium text-gray-900 rounded-md focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}`}
-                      >
-                        <span className="pr-2 leading-none align-middle acc-mob-icon-i sm:absolute top-2/4 -translate-y-2/4">
-                          <i
-                            className={
-                              item.text.toLowerCase() + ' ' + 'sprite-icon'
-                            }
-                          ></i>
-                        </span>
+      <div className="w-full px-0 mx-auto accunt-main-container sm:px-0 lg:px-0">
+        <div className="grid w-full grid-cols-12 px-4 pt-16 sm:px-2 sm:pr-0 main-account-grid">
+          <div className="col-span-12 pl-16 border-r border-gray-200 lg:col-span-4 md:col-span-4 sm:pl-6 tab-list-sm sm:pt-10">
 
-                        {item.text == 'My Profile' ? (
-                          <>
-                          <div
-                          
-                             key={`my-acc-${idx}`}
-                             // href="#"
-                             
-                             className={`ring-white relative ring-opacity-60 border-b border-slate-300 sm:border-0 cursor-pointer ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2  w-full text-14  leading-5 text-left pl-4 ${item.text == "My Saved Address" ? "bg-white border-r-0  sm:border-b-0 sm:border-r-2 sm:border-black font-bold opacity-full" : "text-primary font-medium opacity-60"}`}
+            <div className="sticky left-0 z-10 flex flex-col top-36">
+              {config.map((item: any, idx: number) => (
+                <>
+                  <div
+                    key={`my-acc-${idx}`}
+                    // href="#"
+                    className={`hover:bg-white hover:text-indigo-600 border border-transparent text-md leading-3 font-medium text-gray-900 rounded-md focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"}`}
+                  >
+                    <span className="pr-2 leading-none align-middle acc-mob-icon-i sm:absolute top-2/4 -translate-y-2/4">
+                      <i
+                        className={
+                          item.text.toLowerCase() + ' ' + 'sprite-icon'
+                        }
+                      ></i>
+                    </span>
+
+                    {item.text == 'My Profile' ? (
+                      <>
+                        <div
+
+                          key={`my-acc-${idx}`}
+                          // href="#"
+
+                          className={`ring-white relative ring-opacity-60 border-b border-slate-300 sm:border-0 cursor-pointer ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2  w-full text-14  leading-5 text-left pl-4 ${item.text == "My Saved Address" ? "bg-white border-r-0  sm:border-b-0 sm:border-r-2 sm:border-black font-bold opacity-full" : "text-primary font-medium opacity-60"}`}
+                        >
+                          <span className="pr-2 leading-none align-middle acc-mob-icon-i sm:absolute top-2/4 -translate-y-2/4" >
+                            <i className={item.text.toLowerCase() + ' ' + 'sprite-icon'}></i></span>
+                          <Link
+                            shallow={true}
+                            href={item.href}
+                            passHref
                           >
-                             <span className="pr-2 leading-none align-middle acc-mob-icon-i sm:absolute top-2/4 -translate-y-2/4" >
-                                <i className={item.text.toLowerCase() + ' ' + 'sprite-icon'}></i></span>
-                             <Link
-                                shallow={true}
-                                href={item.href}
-                                passHref
-                             >
-                                <a onClick={() => {
-                                   handleClick;
-                                   setShow(false);
-                                }}
-                                   className="inline-block w-full h-full text-sm sm:pl-8 text-primary px-5 py-5">
-                                   <span className='inline-block sm:hidden'>{item.mtext}</span>
-                                   <span className='hidden sm:inline-block'>{item.text}</span>
-                                </a>
-                             </Link>
-                          </div>
-                       </>
-                        ) : (
-                          <>
-                            <Link shallow={true} href={item.href} passHref>
-                              <a
-                                onClick={() => {
-                                  handleClick
-                                }}
-                                className="inline-block w-full h-full py-4 text-sm sm:pl-8 text-primary"
-                              >
-                                <span className="inline-block sm:hidden">
-                                  {item.mtext}
-                                </span>
-                                <span className="hidden sm:inline-block">
-                                  {item.text}
-                                </span>
-                              </a>
-                            </Link>
-                          </>
-                        )}
-                      </div>
-                    </>
-                  ))}
-                </div>
-              </div>
-
-              <div
-                className={`relative col-span-12 lg:col-span-8 md:col-span-8 border-l tabpanel-sm mob-tab-full ${
-                  isShow ? `mob-hidden` : ''
-                }`}
-              >
-                <div className={'orders bg-white my-2 sm:my-6'}>
-                  <MyDetails />
-                </div>
-              </div>
+                            <a onClick={() => {
+                              handleClick;
+                              setShow(false);
+                            }}
+                              className="inline-block w-full h-full px-5 py-5 text-sm sm:pl-8 text-primary">
+                              <span className='inline-block sm:hidden'>{item.mtext}</span>
+                              <span className='hidden sm:inline-block'>{item.text}</span>
+                            </a>
+                          </Link>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <Link shallow={true} href={item.href} passHref>
+                          <a
+                            onClick={() => {
+                              handleClick
+                            }}
+                            className="inline-block w-full h-full py-4 text-sm sm:pl-8 text-primary"
+                          >
+                            <span className="inline-block sm:hidden">
+                              {item.mtext}
+                            </span>
+                            <span className="hidden sm:inline-block">
+                              {item.text}
+                            </span>
+                          </a>
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </>
+              ))}
             </div>
-    
-     </div>
+          </div>
+
+          <div
+            className={`relative col-span-12 lg:col-span-8 md:col-span-8 border-l tabpanel-sm mob-tab-full ${isShow ? `mob-hidden` : ''
+              }`}
+          >
+            <div className={'orders bg-white my-2 sm:my-6'}>
+              <MyDetails />
+            </div>
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
